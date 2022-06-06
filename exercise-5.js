43c945da8b438fa1294c34c6edaeb2d552db3fcf
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-06-06 13:12:25 +0200

    first commit

---
+++ exercise-5.js
@@ -0,0 +1,11 @@
+const person = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+const { id, firstName: nameing, lastName: surname, age: old } = person;
+console.log(id, nameing, surname, old);
+
+// Ho rinominato 'name' con 'nameing' perchè altrimenti mi diceva che 'name è deprecato
